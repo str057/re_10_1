b974f4d4e35c298412fa8bc02bfc30df3795e24b
RuslanZubrin <error.blin@gmail.com>
2025-03-24 16:55:01 +0300

    1 коммит инициализация и добавление гитигноре

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+print(get_mask_card_number("0000000000000000"))
---
+++ src/masks.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str]) -> Union[str]:
+    """Функция принимает на вход номер карты в виде числа и
+    возвращает маску номера по правилу XXXX XX** **** XXXX"""
+    if card_number.isdigit() and len(card_number) == 16:
+        masked_number = card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+        return masked_number
+    else:
+        return "Проверьте правильность введенного номера карты!"
+
+def get_mask_account(account_number:Union[str]) -> Union[str]:
+    pass
